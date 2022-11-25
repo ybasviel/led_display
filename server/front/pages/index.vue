51aabfb6b6102db3a83a98ae5b4ed6e38ee40d57
VOID <kouichi.hirachi@gmail.com>
2022-11-25 18:37:06 +0900

    add front

---
+++ server/front/pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+<div>
+  <h1>Koken電光掲示板</h1>
+  <p v-html="str"></p>
+  <input type="text" v-model="input">
+  <button @click="send">送信</button>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import axios from 'axios'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  data(){
+    return{
+      str: "",
+      input: ""
+    }
+  },
+  methods:{
+    async send(){
+      if(this.input==""){
+        alert("Input something!");
+      }
+      const data = (await axios.post(`http://lcd.home.k1h.dev:8000/json/${this.input}`));
+      if(data.status==200){
+        console.log("OK")
+        this.update();
+      }else{
+        console.log("Error")
+      }
+    },
+    async update(){
+      this.str="";
+    const data = (await axios.get("http://lcd.home.k1h.dev:8000/json")).data;
+    let lcdOutput:number[] = new Array();
+    let i:number,j:number;
+    for(j=0;j<8;j++){
+      let cal = 1 << j;
+      for(i=0;i<data.data.length;i++){
+        if(data.data[i]&cal){
+          this.str+="●";
+        }else{
+          this.str+="○";
+        }
+      }
+      this.str+="<br>";
+    }
+    }
+  },
+  async mounted() {
+   this.update();
+  }
+})
+</script>
+
+<style>
+body{
+font-family:monospace;
+}
+</style>
