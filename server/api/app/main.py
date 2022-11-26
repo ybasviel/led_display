6240cf9d528f9e72075ea5dfa42caf2c78dd6096
VOID <kouichi.hirachi@gmail.com>
2022-11-26 16:36:52 +0900

    add cors

--- server/api/app/main.py
+++ server/api/app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from app.generate import getJSON
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://lcd.home.k1h.dev"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def hello():
--- server/front/nuxt.config.js
+++ server/front/nuxt.config.js
@@ -1,9 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'front',
+    title: 'Koken 電光掲示板',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ja'
     },
     meta: [
       { charset: 'utf-8' },
